8c69dd6dfcff96390d928d6d011cd06224459308
reza <reza@graveltechnology.com>
2020-12-17 11:53:18 +0700

    Learning Rate momentum

---
+++ 2020/neuron_from_scratch/10_06_momentum.py
@@ -0,0 +1,81 @@
+# PAGE 44
+import numpy as np
+import matplotlib.pyplot as plt
+import nnfs
+from nnfs.datasets import spiral_data
+
+from libraries import Activation_Softmax_Loss_CategoricalCorssentropy
+from libraries import Activation_Softmax
+from libraries import Loss_CategoricalCrossentropy
+from libraries import Activation_ReLU
+from libraries import Layer_Dense
+from libraries import Optimizer_SGD_version3
+
+
+# initialize nnfs dataset
+nnfs.init()
+
+
+# Create dataset
+x, y = spiral_data(samples=100, classes=3)
+
+#  Create dense layer with 2 input features and 64 output valus
+dense1 = Layer_Dense(2, 64)
+
+#  Create ReLU activation (to be used with Dense Layer).
+activation_relu = Activation_ReLU()
+
+# Create second dense layer with 64 iput features ( as we take output of previous layer here)
+# and 3 output values
+dense2 = Layer_Dense(64, 3)
+
+# Create softmax classfier's combined loss and activation
+loss_activation = Activation_Softmax_Loss_CategoricalCorssentropy()
+
+# Create optimizer kita menggunakan decay dan momentum untuk mendapatkan LR global minimum
+optimizer = Optimizer_SGD_version3(learning_rate=.9,decay=1e-3,  momentum=0.5)
+
+# Train in loop
+for epoch in range(10001):
+
+    # Perform a forward pass of our training data through this layer
+    dense1.forward(x)
+
+    # perform a forward pass through activation function
+    # takes the output of first dense layer here
+    activation_relu.forward(dense1.output)
+
+
+    # Perform a forward pass through second dense layer
+    # takes output of activation function of first layer as inputs
+    dense2.forward(activation_relu.output)
+
+    # Perform a forward pass through second Dense layer
+    # takes the output of second dense layer here and return loss
+    loss = loss_activation.forward(dense2.output, y)
+
+    # Calculate accuracy from output of activation loss and target calculate values along first axis
+    predictions = np.argmax(loss_activation.output, axis=1)
+    if len(y.shape) == 2:
+        y = np.argmax(y, axis=1)
+
+    accuracy = np.mean(predictions == y)
+    if not epoch % 100:
+        print(f'epoch: {epoch}, ' +
+              f'acc: {accuracy:.3f}, ' +
+              f'loss: {loss:.3f}, ' +
+              f'lr: {optimizer.current_learning_rate}')
+
+    # Backward pass/ backpropagation
+    loss_activation.backward(loss_activation.output, y)
+    dense2.backward(loss_activation.dinputs)
+    activation_relu.backward(dense2.dinputs)
+    dense1.backward(activation_relu.dinputs)
+
+    # Then we finnaly use our optimizer to update weights and biases
+
+    # Update weight and biases
+    optimizer.pre_update_params()
+    optimizer.update_params(dense1)
+    optimizer.update_params(dense2)
+    optimizer.post_update_params()
--- 2020/neuron_from_scratch/libraries.py
+++ 2020/neuron_from_scratch/libraries.py
@@ -248,3 +248,59 @@ class Optimizer_SGD_version2:
 
   def post_update_params(self):
     self.iterations += 1
+
+# Optimizer_SGD version 3 with momentum
+class Optimizer_SGD_version3:
+  # Initialize optimizer - set settings,
+  # learning rate of 1. is default for this optimizer
+  def __init__(self, learning_rate=1.0, decay=0., momentum=0.):
+    self.starting_learing_rate = learning_rate
+    self.current_learning_rate = learning_rate
+    self.decay = decay
+    self.iterations = 0
+    self.momentum = momentum
+
+  # call once before any parameter updates
+  def pre_update_params(self):
+    # if optimizer use Decay Learning Rate
+    if self.decay:
+      self.current_learning_rate = self.starting_learing_rate * (1. / (1 + self.decay * self.iterations))
+
+  # Update parameters
+  def update_params(self, layer):
+    # If we use momentum
+    if self.momentum:
+
+      # If layer does not contain momentum arrays, create them  array filled with zeros
+      if not hasattr(layer, 'weight_momentums'):
+        layer.weight_momentums = np.zeros_like(layer.weights)
+
+        # If there is no momentum array for weights
+        # The array doesn't exists for biases yet either
+        layer.bias_momentums = np.zeros_like(layer.biases)
+
+      # Build weight updates with momentum - take previous
+      # updates multiplied by retain factor and update with current gradient
+      weight_updates = self.momentum * layer.weight_momentums - \
+        self.current_learning_rate * layer.dweights
+
+      # Build bias updates
+      bias_updates = self.momentum * layer.bias_momentums - \
+        self.current_learning_rate * layer.dbiases
+
+      layer.weight_momentums = weight_updates
+      layer.bias_momentums = bias_updates
+    
+    # Vanila SGD update (as before momentum update)
+    else:
+      weight_updates = -self.current_learning_rate * layer.dweights
+      bias_updates = -self.current_learning_rate * layer.dbiases
+
+    # Update weights and biases using either
+    # vanilla or momentum updates
+    layer.weights += weight_updates
+    layer.biases += bias_updates
+
+  def post_update_params(self):
+    self.iterations += 1
+
